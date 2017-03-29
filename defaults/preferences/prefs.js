b40018b32c5693813e2378eb55dba1db283d72d1
JustOff <Off.Just.Off@gmail.com>
2017-03-29 16:34:56 +0300

    cleanup

--- defaults/preferences/prefs.js
+++
@@ -1,3 +0,0 @@
-pref("extensions.jid1-UReeVGBoB5LmIw@jetpack.openDelay", 0);
-pref("extensions.jid1-UReeVGBoB5LmIw@jetpack.closeDelay", 0);
-pref("extensions.jid1-UReeVGBoB5LmIw@jetpack.approach", "A");
