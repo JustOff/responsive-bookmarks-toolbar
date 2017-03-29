594775ffb1c456bb25baf7d1b83380b24452dc97
JustOff <Off.Just.Off@gmail.com>
2017-03-29 16:33:16 +0300

    import from Mouseover Bookmarks Toolbar 1.1.1

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,3 @@
+pref("extensions.jid1-UReeVGBoB5LmIw@jetpack.openDelay", 0);
+pref("extensions.jid1-UReeVGBoB5LmIw@jetpack.closeDelay", 0);
+pref("extensions.jid1-UReeVGBoB5LmIw@jetpack.approach", "A");
