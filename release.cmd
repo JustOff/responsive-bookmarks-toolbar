c0e537dd2774867762470e3ecbff354e5a4c5e9e
JustOff <Off.Just.Off@gmail.com>
2021-02-22 19:28:13 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=2.0.1
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/responsive-bookmarks-toolbar-.+?\.xpi/download\/%VER%\/responsive-bookmarks-toolbar-%VER%\.xpi/" update.xml
+
+set XPI=responsive-bookmarks-toolbar-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe
