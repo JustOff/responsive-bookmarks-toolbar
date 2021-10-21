ea637769bff9465cc031831e12b427c548077f64
JustOff <Off.Just.Off@gmail.com>
2021-10-21 14:48:10 +0300

    Bump version

--- release.cmd
+++ release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VER=2.0.3
+set VER=2.0.4
 
 sed -i -E "s/version>.+?</version>%VER%</" install.rdf
 sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/responsive-bookmarks-toolbar-.+?\.xpi/download\/%VER%\/responsive-bookmarks-toolbar-%VER%\.xpi/" update.xml
